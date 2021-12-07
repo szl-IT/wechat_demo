4b19fa236fcee103160930f3031a4fa7f01e33c9
songzhiling <szl17630035658@gmail.com>
2021-12-07 09:43:49 +0800

    commit
    初次提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,9 +13,10 @@ Page({
   },
   // 事件处理函数
   bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    this.getRequest(),
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
   },
   onLoad() {
     if (wx.getUserProfile) {
@@ -37,12 +38,11 @@ Page({
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
+  getRequest() {
+    wx.login({
+      success: res => { console.log(res) }
+    },
+    )
+  },
 })
